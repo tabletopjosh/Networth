78b7512ff7e2468fa4a4e420fc5e6eeeb1eb8fcb
Jun-Li-dev <Jun_li.lin@uconn.edu>
2026-04-17 17:59:43 +0000

    did calc yes

--- main.py
+++ main.py
@@ -1,10 +1,39 @@
-def calc_hustlescore(totalpoints = list(), months=1, frozen_months=0):
-    points = 0
-    for i in totalpoints:
-        points += i
-        lifetime = points/(months-frozen_months)
-    monthsinyear = len(totalpoints)%12-frozen_months
-    monthly = totalpoints[-1]/100
-    yearly = totalpoints[-12:]/100*monthsinyear
+#def calc_hustlescore(totalpoints = list(), months=1, frozen_months=0):
+def calc_hustlescore(totalpoints=None, months=1, frozen_months=0):
+    if totalpoints is None:
+        totalpoints = []
+
+    if months - frozen_months <= 0:
+        raise ValueError("months must be greater than frozen_months")
+
+    points = sum(totalpoints)
+
+    # Lifetime average
+    lifetime = points / (months - frozen_months)
+
+    # Monthly (last entry)
+    monthly = totalpoints[-1] / 100 if totalpoints else 0
+
+    # Yearly (last up to 12 months)
+    last_12 = totalpoints[-12:]
+    active_months = max(len(last_12) - frozen_months, 0)
+
+    if active_months > 0:
+        yearly = sum(last_12) / 100
+    else:
+        yearly = 0
+
     return monthly, yearly, lifetime
+    
+   # points = 0
+    #yearly
+    #for i in totalpoints:
+     #   points += i
+    #lifetime = points/(months-frozen_months)
+    #if len(totalpoints)%12-frozen_months>0:
+     #   monthsinyear = len(totalpoints)%12-frozen_months
+      #  yearly = totalpoints[-12:]/100*monthsinyear
+
+    #monthly = totalpoints[-1]/100
+    #return monthly, yearly, lifetime
 
