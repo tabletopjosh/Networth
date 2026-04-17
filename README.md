6806ac0d8d405ac05aeae178e32bab346c85cc67
Jun-Li-dev <Jun_li.lin@uconn.edu>
2026-04-17 17:50:35 +0000

    did calc

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # Networth
+
+
+to merge locally and push
+
+# switch to main
+git checkout main
+
+# make sure it's up to date
+git pull origin main
+
+# merge your branch
+git merge your-branch-name
+
+# push updated main
+git push origin main
--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("Hello World")
+def calc_hustlescore(totalpoints = list(), months=1, frozen_months=0):
+    points = 0
+    for i in totalpoints:
+        points += i
+        lifetime = points/(months-frozen_months)
+    monthsinyear = len(totalpoints)%12-frozen_months
+    monthly = totalpoints[-1]/100
+    yearly = totalpoints[-12:]/100*monthsinyear
+    return monthly, yearly, lifetime
+
